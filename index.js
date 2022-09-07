5e865d526ac8de7862aeb7d88063623773239fdd
Duncan Gatuthu <dgatuthu@gmail.com>
2022-09-07 09:30:48 +0300

    Handle GEt product search endpoint

--- index.js
+++ index.js
@@ -60,4 +60,18 @@ app.get('/products/:productId/offers', async (req, res) => {
   }
 })
 
+// GET Search results
+app.get('/search/:searchQuery', async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`)
+
+    res.json(JSON.parse(response))
+
+  } catch (error) {
+    res.json(error)
+  }
+})
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
